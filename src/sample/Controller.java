ec11f422d88deda5f78b9144b0a93b37ed0d53c4
Abhishek Gupta <abhishek16004@iiitd.ac.in>
2017-10-17 20:14:51 +0530

    Added Login page

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,15 @@
 package sample;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 public class Controller {
+    private Stage stage;
+    private Scene scene;
+    private Parent parent;
+    public Controller(){
+
+    }
 }
---
+++ src/sample/LoginController.java
@@ -0,0 +1,31 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+
+import java.awt.*;
+import java.util.concurrent.ExecutionException;
+
+import javafx.scene.control.TextField;
+import javafx.scene.control.Label;
+import javafx.event.ActionEvent;
+public class LoginController {
+    @FXML
+    private TextField txtUserName;
+    @FXML
+    private PasswordField tstPassword;
+    @FXML
+    private Label txtLabel;
+    @FXML
+    public void login(ActionEvent event){
+        try {
+            if (txtUserName.getText().equals("user") && tstPassword.getText().equals("pass")) {
+                txtLabel.setText("Success");
+            } else {
+                txtLabel.setText("Failure");
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
