63e6abec48749a9f30cdc3d45c5ad21844fb12ac
Abhishek Gupta <abhishek16004@iiitd.ac.in>
2017-10-24 05:30:03 +0530

    Added PendingRequest (table)

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,20 +4,18 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
 import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-    Group root;
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Node x = FXMLLoader.load(getClass().getResource("/sample/MenuBar.fxml"));
-        Node y = FXMLLoader.load(getClass().getResource("/sample/TimeTable.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/sample/PendingRequest.fxml"));
 
-        root = new Group();
-        root.getChildren().addAll(x, y);
+
         primaryStage.setTitle("Application");
-        primaryStage.setScene(new Scene(root, 6000, 500));
+        primaryStage.setScene(new Scene(root, 600, 500));
 
         primaryStage.show();
     }
