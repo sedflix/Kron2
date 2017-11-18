8f542f32b6769c9c0f8e2dc7bec8168170dc1b32
Abhishek Gupta <abhishek16004@iiitd.ac.in>
2017-11-18 16:11:25 +0530

    courseView with credit and audit

--- src/sample/CourseViewController.java
+++ src/sample/CourseViewController.java
@@ -1,17 +1,14 @@
 package sample;
 
-import api.Course;
-import api.Faculty;
-import api.User;
+import api.*;
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.ListCell;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.TextArea;
+import javafx.scene.control.*;
 import javafx.scene.input.KeyCode;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextFlow;
 import javafx.stage.Stage;
@@ -25,6 +22,8 @@ import java.util.Set;
 public class CourseViewController extends Application{
     private User user;
     private Parent courseDescription;
+    Course temp;
+
     public static void main(String[] args) {
         launch(args);
     }
@@ -55,17 +54,29 @@ public class CourseViewController extends Application{
                 }
             };
 
-            cell.setOnMousePressed(e -> {
-                if (! cell.isEmpty()) {
-                    Course forInfo = Course.getCourseByName(cell.getItem());
+            cell.setOnMousePressed(new EventHandler<MouseEvent>() {
+                @Override
+                public void handle(MouseEvent event) {
+                        if (! cell.isEmpty()) {
+                            Course forInfo = Course.getCourseByName(cell.getItem());
+                            setTemp(forInfo);
+                            setData(courseDescription, forInfo);
+                        }
 
-                    setData(courseDescription, forInfo);
                 }
             });
             return cell ;
         });
 
+        Button creditButton = (Button) courseDescription.lookup("#creditButton");
+        creditButton.setOnMouseClicked(event -> {
+            creditControl();
+        });
 
+        Button auditButton = (Button) courseDescription.lookup("#auditButton");
+        auditButton.setOnMouseClicked(event -> {
+            auditControl();
+        });
 
 //        primaryStage.setScene(new Scene(courseDescription, 700, 900));
 //        primaryStage.show();
@@ -115,5 +126,27 @@ public class CourseViewController extends Application{
 
     public void setUser(User user) {
         this.user = user;
+    }
+
+    public void setTemp(Course course){
+        temp=course;
+    }
+    public void creditControl(){
+        if (user.getDtype().equals("Student")) {
+            ((Student) user).insertRegisteredCourse(temp);
+        } else if (user.getDtype().equals("Faculty")) {
+
+        } else if (user.getDtype().equals("Admin")) {
+
+        }
+    }
+    public void auditControl(){
+        if (user.getDtype().equals("Student")) {
+            ((Student) user).insertAuditedCourse(temp);
+        } else if (user.getDtype().equals("Faculty")) {
+
+        } else if (user.getDtype().equals("Admin")) {
+
+        }
     }
 }
